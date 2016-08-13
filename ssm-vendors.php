15fda1abaa01bb3d46e9c502cbf7647cacd72ce9
Rich Staats <rich@secretstache.com>
2016-08-13 15:06:01 -0600

    adds support for kernl continuous integration

--- ssm-vendors.php
+++ ssm-vendors.php
@@ -9,7 +9,7 @@
  * Plugin Name: SSM Vendors
  * Plugin URI:  http://secretstache.com
  * Description: Enables a Vendor Custom Post Type.
- * Version:     0.1.0
+ * Version:     0.1.1
  * Author:      Secret Stache Media
  * Author URI:  http://secretstache.com
  * Text Domain: ssm-vendors
